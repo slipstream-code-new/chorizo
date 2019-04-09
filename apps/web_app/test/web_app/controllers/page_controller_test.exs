e780bbb2aacd16ca73c761134d0cb5e2be3daac5
John Wilger <john@johnwilger.com>
2019-04-09 09:47:15 -0700

    Removes bad and useless test

--- apps/web_app/test/web_app/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Chorizo.WebApp.PageControllerTest do
-  use Chorizo.WebApp.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
