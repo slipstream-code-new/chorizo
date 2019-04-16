133d1cb9287f61846ccda09e9f77beb316967558
LTheGreats <lachlan@wilgerfamily.com>
2019-04-15 22:39:03 -0700

    Adds basic UI for login, no connections to endpoints yet.

--- apps/web_app/assets/src/Main.elm
+++ apps/web_app/assets/src/Main.elm
@@ -2,7 +2,11 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation exposing (Key)
-import Element exposing (Element, el, text)
+import Element exposing (Element, el, text, column, spacing, centerY, padding, rgb255, centerX)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
 import Url exposing (Url)
 
 
@@ -11,12 +15,20 @@ import Url exposing (Url)
 
 
 type alias Model =
-    { loggedIn : Bool }
+  { loggedIn : Bool
+  , emailInput : String
+  , passwordInput : String
+  , submitted : Bool
+  }
 
 
 initialModel : Model
 initialModel =
-    { loggedIn = False }
+  { loggedIn = False
+  , emailInput = ""
+  , passwordInput = ""
+  , submitted = False
+  }
 
 
 
@@ -25,29 +37,73 @@ initialModel =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Chorizo"
-    , body =
-        [ Element.layout []
-            (el [] (text "Hello, World!"))
-        ]
-    }
+  { title = "Chorizo"
+  , body =
+      [ Element.layout []
+          (loginForm model)
+      ]
+  }
 
+loginForm : Model -> Element Msg
+loginForm model =
+  column [ spacing 30, centerY, centerX]
+    [ text "Login Please"
+    , Input.email []
+        { onChange = EmailInputChanged
+        , text = model.emailInput
+        , placeholder = Just (Input.placeholder [] (text "email"))
+        , label = Input.labelHidden "Email"
+        }
+    , Input.newPassword []
+        { onChange = PasswordInputChanged
+        , text = model.passwordInput
+        , placeholder = Just(Input.placeholder [] (text "password"))
+        , label = Input.labelHidden "Email"
+        , show = False
+        }
+    , Input.button []
+        { onPress = Just SubmitLogin
+        , label = (el 
+          [ Background.color (rgb255 50 0 40)
+          , Border.rounded 3
+          , Font.color (rgb255 255 255 255)
+          , padding 10
+          ] (text "Login"))
+        }
+    ]
 
 
 --> Update
 
 
 type Msg
-    = UrlRequested Browser.UrlRequest
-    | UrlChanged Url
+  = UrlRequested Browser.UrlRequest
+  | UrlChanged Url
+  | PasswordInputChanged String
+  | EmailInputChanged String
+  | SubmitLogin
 
 
 update msg model =
-    ( model, Cmd.none )
+  case msg of
+    EmailInputChanged text ->
+      ({ model | emailInput = text }, Cmd.none)
+
+    PasswordInputChanged text ->
+      ({ model | passwordInput = text }, Cmd.none)
+
+    UrlRequested request ->
+      (model, Cmd.none)
+
+    UrlChanged url ->
+      (model, Cmd.none)
+
+    SubmitLogin ->
+      ({ emailInput = "", passwordInput = "", loggedIn = False, submitted = True }, Cmd.none)
 
 
 subscriptions model =
-    Sub.none
+  Sub.none
 
 
 
@@ -56,16 +112,16 @@ subscriptions model =
 
 init : flags -> Url -> Key -> ( Model, Cmd msg )
 init flags url key =
-    ( initialModel, Cmd.none )
+  ( initialModel, Cmd.none )
 
 
 main : Program () Model Msg
 main =
-    Browser.application
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        , onUrlRequest = UrlRequested
-        , onUrlChange = UrlChanged
-        }
+  Browser.application
+      { init = init
+      , view = view
+      , update = update
+      , subscriptions = subscriptions
+      , onUrlRequest = UrlRequested
+      , onUrlChange = UrlChanged
+      }
